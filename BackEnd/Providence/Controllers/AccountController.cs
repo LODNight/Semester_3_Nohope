8f3a6054326a96ef51018044060d3ad8209ad986
LODNight <khuunhattin123@gmail.com>
2023-07-03 13:59:53 +0700

    af Get

--- BackEnd/Providence/Controllers/AccountController.cs
+++ BackEnd/Providence/Controllers/AccountController.cs
@@ -20,7 +20,7 @@ public class AccountController : Controller
 
 
     // ===============================
-    // ============== GET
+    // ============== GET GET
     // ===============================
 
     // Find All
